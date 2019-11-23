c23a887cc4a2f1443b0a5a4ae9b1e25eb46d2223
Mahirobot <mahirajalisha@gmail.com>
2019-11-23 14:04:40 +0600

    Multichat platform using socket programming

---
+++ Client.java
@@ -0,0 +1,81 @@
+import java.io.DataInputStream;
+import java.io.PrintStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client implements Runnable {
+
+  // The client socket
+  private static Socket clientSocket = null;
+  // The output stream
+  private static PrintStream os = null;
+  // The input stream
+  private static DataInputStream is = null;
+
+  private static BufferedReader inputLine = null;
+  private static boolean closed = false;
+  
+  public static void main(String[] args) {
+
+    // The default port.
+    int portNumber = 5000;
+    // The default host.
+    String host = "192.168.1.6";
+
+    if (args.length < 2) {
+      System.out
+          .println("Usage: java MultithreadClient <host> <portNumber>\n"
+              + "Now using host=" + host + ", portNumber=" + portNumber);
+    } else {
+      host = args[0];
+      portNumber = Integer.valueOf(args[1]).intValue();
+    }
+
+    try {
+      clientSocket = new Socket(host, portNumber);
+      inputLine = new BufferedReader(new InputStreamReader(System.in));
+      os = new PrintStream(clientSocket.getOutputStream());
+      is = new DataInputStream(clientSocket.getInputStream());
+    } catch (UnknownHostException e) {
+      System.err.println("Don't know about host " + host);
+    } catch (IOException e) {
+      System.err.println("Couldn't get I/O for connection the host "
+          + host);
+    }
+
+    if (clientSocket != null && os != null && is != null) {
+      try {
+
+        new Thread(new Client()).start();
+        while (!closed) {
+          os.println(inputLine.readLine().trim());
+        }
+       
+        os.close();
+        is.close();
+        clientSocket.close();
+      } catch (IOException e) {
+        System.err.println("IOException:  " + e);
+      }
+    }
+  }
+
+  
+  public void run() {
+    
+    String responseLine;
+    try {
+      while ((responseLine = is.readLine()) != null) {
+        System.out.println(responseLine);
+        if (responseLine.indexOf("GoodBye!!!") != -1)
+          break;
+      }
+      closed = true;
+    } catch (IOException e) {
+      System.err.println("IOException:  " + e);
+    }
+  }
+}
